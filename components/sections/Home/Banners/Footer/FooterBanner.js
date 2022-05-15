a426508923aaada8f0662fd4b382373f52716869
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-05-15 13:59:07 +0200

    Footer Banner implemented

---
+++ components/sections/Home/Banners/Footer/FooterBanner.js
@@ -0,0 +1,41 @@
+import styles from './FooterBanner.module.css'
+import Link from 'next/link'
+
+import { Banner } from '../../../../common/Banner/Banner'
+import { Button } from '../../../../common/Button'
+import { Image } from '../../../../common/Image'
+import { urlFor } from '../../../../../config/SanityClient'
+
+export const FooterBanner = ({ product }) => {
+  console.log('product... ', product)
+  const { description, discount, discountDate, _id, image, name, saleTime } = product
+
+  return (
+    <Banner className={styles.bannerContainer}>
+      <div className={styles.bannerDescription}>
+        <div className={styles.left}>
+          <h3>{name}</h3>
+          <p>{description}</p>
+          <Link href={`/product/${_id}`}>
+            <Button className={styles.bannerButton} type='submit'>
+              Comprar ahora!
+            </Button>
+          </Link>
+        </div>
+        <div className={styles.right}>
+          <h3>{discount}</h3>
+          <h3>{saleTime}</h3>
+          <h3>{discountDate}</h3>
+        </div>
+      </div>
+
+      <Image 
+        alt={name} 
+        className={styles.bannerImage}
+        height={500}
+        src={urlFor(image)} 
+        width={500}
+      />
+    </Banner>
+  )
+}
--- components/sections/Home/Banners/FooterBanner.js
+++
@@ -1,9 +0,0 @@
-import { Banner } from '../../../common/Banner/Banner'
-
-export const FooterBanner = () => {
-  return (
-    <Banner>
-      Hola
-    </Banner>
-  )
-}
