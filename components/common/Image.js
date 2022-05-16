25ca90dbc0e759ac3095bbe4969ef1fc33c82759
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-05-16 12:42:15 +0200

    Layout styles

--- components/common/Image.js
+++ components/common/Image.js
@@ -1,9 +1,10 @@
-export const Image = ({ alt, className, height, src, width }) => {
+export const Image = ({ alt, className, height, onMouseEnter, src, width }) => {
   return (
     <img 
       alt={alt}
       className={className}
       height={height}
+      onMouseEnter={onMouseEnter}
       src={src}
       width={width}
     />
