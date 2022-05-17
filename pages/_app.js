fde77e2beed448f7a59ade1b390374d18b349817
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-05-17 12:05:03 +0200

    Types & CartState App Wrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import '../styles/globals.css'
+import { Toaster } from 'react-hot-toast'
 
 import { Layout } from '../components/common/Layout/Layout'
+import { CartState } from '../context/Cart/CartState'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <CartState>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </CartState>
   )
 }
 
