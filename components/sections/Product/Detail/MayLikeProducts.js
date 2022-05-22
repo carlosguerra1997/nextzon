5d37ea2a423ad1e1aa30f005b10c235527771af6
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-05-22 13:12:23 +0200

    MayLikeProducts fix

--- components/sections/Product/Detail/MayLikeProducts.js
+++ components/sections/Product/Detail/MayLikeProducts.js
@@ -1,17 +1,24 @@
 import styles from './ProductDetail.module.css'
+import { useEffect, useState } from 'react'
 
 import { Product } from '../Product'
+import { getRandomProducts } from '../../../../helpers/index'
+
 
 export const MayLikeProducts = ({ products, currentProduct }) => {
+  const [ mayLikeProducts, setMayLikeProducts ] = useState([])
+
+  useEffect(() => {
+    setMayLikeProducts(getRandomProducts(products, currentProduct))
+  }, [ currentProduct ])
+  
   return (
     <div className={styles.maylikeProductsWrapper}>
       <h2>Productos que también puedan interesarte</h2>
       <div className={styles.maylikeProductsContainer}>
         {
-          products.map(product => {
-            if (product.slug.current !== currentProduct) {
-              return <Product key={product._id} {...product} />
-            }
+          mayLikeProducts.map(product => {
+            return <Product key={product._id} {...product} />
           })
         }
       </div>
